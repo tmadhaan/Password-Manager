6a0213df6efecc2aa31373675896838369b4ca8c
tmadhaan <140404243+tmadhaan@users.noreply.github.com>
2026-03-28 22:43:08 +0000

    main.py

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,6 +1,47 @@
-import hashlib
+import base64
+import os
 import random
 import string
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+PASSWORD_FILE = "passwords.txt"
+SALT_FILE = "salt.bin"
+CHECK_FILE = "check.bin"
+
+def get_salt():
+    if not os.path.exists(SALT_FILE):
+        salt = os.urandom(16)
+        with open(SALT_FILE, "wb") as f:
+            f.write(salt)
+    else:
+        with open(SALT_FILE, "rb") as f:
+            salt = f.read()
+    return salt
+
+def derive_key(password):
+    salt = get_salt()
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000,)
+    return base64.urlsafe_b64encode(kdf.derive(password.encode()))
+
+def get_cipher(master_password):
+    key = derive_key(master_password)
+    return Fernet(key)
+
+def verify_master_password(cipher):
+    if not os.path.exists(CHECK_FILE):
+        # first time setup master password is "admin123"
+        with open(CHECK_FILE, "wb") as f:
+            f.write(cipher.encrypt(b"verified"))
+        return True
+    else:
+        try:
+            with open(CHECK_FILE, "rb") as f:
+                cipher.decrypt(f.read())
+            return True
+        except:
+            return False
 
 def menu():
     print("\n--- Password Manager ---")
@@ -9,9 +50,6 @@ def menu():
     print("3. Search Password")
     print("4. Delete Password")
     print("5. Exit")
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
 
 def generate_password():
     length = 12
@@ -22,94 +60,100 @@ def generate_password():
     
 
 def login():
-    master_hash = hash_password("admin123")
-    attempt = input("Enter master password: ")
+    return input("Enter master password: ")
 
-    if hash_password(attempt) != master_hash:
-        print("Incorrect password. Access denied.")
-        return False
-    return True
-
-def add_password():
+def add_password(cipher):
     website = input("Enter website: ")
     username = input("Enter username: ")
     choice = input("Generate password? (y/n): ")
 
-    if choice == "y":
+    if choice.lower() == "y":
         password = generate_password()
     else:
         password = input("Enter password: ")
 
-    with open("passwords.txt", "a") as file:
-        file.write(f"Website: {website} | Username: {username} | Password: {password}\n")
+    encrypted = cipher.encrypt(password.encode())
+
+    with open(PASSWORD_FILE, "ab") as file:
+        line = f"{website}|{username}|".encode() + encrypted + b"\n"
+        file.write(line)
+
     print("Password has been saved.")
 
-def view_passwords():
+def view_passwords(cipher):
     try:
-        with open("passwords.txt", "r") as file:
-            lines = file.readlines()
+        with open(PASSWORD_FILE, "rb") as file:
+           print("Saved Passwords")
+           for line in file:
+            parts = line.strip().split(b"|")
+            website = parts[0].decode()
+            username = parts[1].decode()
+            encrypted_password = parts[2]
+            
+            decrypted = cipher.decrypt(encrypted_password).decode()
+            print(f"{website} | {username} | {decrypted}")
 
-            if not lines:
-                print("\n No passwords saved currently.\n")
-            else:
-                print("\n--- Saved Passwords ---")
-                for line in lines:
-                    print(line.strip())
     except FileNotFoundError:
-        print("\n No passwords saved currently.\n")
+        print("There are no passwords saved.")
 
-def search_password():
-    search = input("Enter website to search: ")
+def search_password(cipher):
+    search = input("ENter a website to search: ").lower()
 
     try:
-        with open("passwords.txt", "r") as file:
+        with open(PASSWORD_FILE, "rb") as file:
             found = False
             for line in file:
-                if search.lower() in line.lower():
-                    print(line.strip())
+                parts = line.strip().split(b"|")
+                website = parts[0].decode()
+
+                if search in website.lower():
+                    username = parts[1].decode()
+                    decrypted = cipher.decrypt(parts[2]).decode()
+                    print(f"{website} | {username} | {decrypted}")
                     found = True
+
             if not found:
                 print("No matching results.")
-
     except FileNotFoundError:
-        print("No passwords saved currently.")
+        print("There are no passwords saved.")
 
-def delete_password():
-    website = input("Enter website to delete: ")
-
-    try: 
-        with open("passwords.txt", "r") as file:
+def delete_password(cipher):
+    website_delete = input("Enter website to delete: ").lower()
+    try:
+        with open(PASSWORD_FILE, "rb") as file:
             lines = file.readlines()
 
-        with open("passwords.txt", "w") as file:
+        with open(PASSWORD_FILE, "wb") as file:
             found = False
             for line in lines:
-                if website.lower() not in line.lower():
+                parts = line.strip().split(b"|")
+                website = parts[0].decode().lower()
+
+                if website_delete not in website:
                     file.write(line)
                 else:
                     found = True
-        
         if found:
-            print("Password deleted.")
+            print("Password has been deleted.")
         else:
             print("Website not found.")
-    
-    except FileNotFoundError:
-        print("No passwords saved currently.")
 
-def main():
+    except FileNotFoundError:
+        print("There are no passwords saved.")
+
+def main(cipher):
     while True:
         menu()
         choice = input("Choose an option: ")
 
         if choice == "1":
-            add_password()
+            add_password(cipher)
         elif choice == "2":
-            view_passwords()
+            view_passwords(cipher)
         elif choice == "3":
-            search_password()
+            search_password(cipher)
         elif choice == "4":
-            delete_password()
+            delete_password(cipher)
         elif choice == "5":
             print("Goodbye!")
             break
@@ -117,5 +161,10 @@ def main():
             print("Invalid choice.")
 
 if __name__ == "__main__":
-    if login():
-        main()
+    master_password = login()
+    cipher = get_cipher(master_password)
+    
+    if verify_master_password(cipher):
+        main(cipher)
+    else:
+        print("Incorrect master password.")
